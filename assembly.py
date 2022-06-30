e980f8bcd0055be8558cbe6305b0b0f5486103b6
Troy Fullwood <everyfan3417@gmail.com>
2022-06-29 21:37:53 -0400

    large portions of expression output are correct

--- assembly.py
+++ assembly.py
@@ -3,6 +3,8 @@
 from string import whitespace
 from expr import Leaf, Node
 from parse_state import Parser
+import opinfo
+from symtab import Symbol
 
 
 def asm(parser: Parser, line: str) -> None:
@@ -39,13 +41,74 @@ def fasm(parser, line: str, flag: bool) -> None:
 
 def asmexpr(parser: Parser, node: Node) -> None:
     """Assemble an expression tree."""
-    # TODO: assemble expressions
-    for child in node.children:
-        asmexpr(parser, child)
-    if isinstance(node, Leaf):
-        asm(parser, f'{node.label} {node.value}')
-    else:
-        asm(parser, node.label)
+    asm(parser, 'clear')
+    asmnode(parser, node)
+    rval(parser, node)
+
+
+def rval(parser: Parser, node: Node) -> None:
+    """If the node is an lval, do a load operation.
+    """
+    if node.label == 'name':
+        assert isinstance(node, Leaf) and isinstance(node.value, Symbol)
+        if node.value.storage == 'register':
+            asm(parser, f'grabreg {node.value.offset}')
+            return
+    if opinfo.islval[node.label]:
+        match node.typestr[0].type:
+            case 'char':
+                cmd = 'cload'
+            case 'float':
+                cmd = 'fload'
+            case 'double':
+                cmd = 'dload'
+            case _:
+                cmd = 'load'
+        asm(parser, cmd)
+
+
+def asmnode(parser: Parser, node: Node) -> None:
+    """Assemble expression nodes recursively."""
+    match node.label:
+        # Special cases
+        case 'call':
+            for arg in node.children[1:]:
+                asmnode(parser, arg)
+                rval(parser, arg)
+            asmnode(parser, node.children[0])
+            rval(parser, node.children[0])
+            asm(parser, f'call {len(node.children[1:])}')
+            return
+        case 'con' | 'fcon':
+            assert isinstance(node, Leaf)
+            fasm(parser, f'push {node.value}', node.label == 'fcon')
+            return
+        case 'name':
+            assert isinstance(node, Leaf) and isinstance(node.value, Symbol)
+            match node.value.storage:
+                case 'auto':
+                    asm(parser, f'auto {node.value.offset}')
+                case 'static':
+                    asm(parser, f'push {node.value.offset}')
+                case 'extern':
+                    asm(parser, f'push {node.value.name}')
+                case 'register':
+                    return
+                case _:
+                    parser.crash('BAD NAME NODE')
+            return
+    if len(node.children) > 0:
+        asmnode(parser, node.children[0])
+        if not opinfo.needlval[node.label]:
+            rval(parser, node.children[0])
+        for child in node.children[1:]:
+            asmnode(parser, child)
+            rval(parser, child)
+    match node.label:
+        case 'deref' | 'addr':
+            pass
+        case _:
+            asm(parser, node.label)
 
 
 def goseg(parser: Parser, segment: str) -> str:
